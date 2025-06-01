5bc470bfa505552ee7f1b045e29a21bc0b590be1
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-01 11:53:27 +0700

    feat: add conditional rendering in case of user login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
-import { Link } from 'react-router';
-import logo from '../assets/noir-logo.svg'
+import { Link, useLocation, useNavigate } from 'react-router';
 import { LuSquareArrowOutUpRight } from "react-icons/lu";
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutUserAction } from '../redux/reducers/auth';
+import logo from '../assets/noir-logo.svg'
 
 export default function Header({ className }) {
+  const currentUser = useSelector((state) => state.auth.data)
+  const dispatch = useDispatch()
+  let navigate = useNavigate()
+  const location = useLocation()
+
+  const handleLogout = () => {
+    dispatch(logoutUserAction())
+    navigate('/auth/login')
+  }
   return (
     <header>
       <nav className={`flex fixed z-10 w-screen justify-center ${className}`}>
@@ -10,30 +21,44 @@ export default function Header({ className }) {
           <a href="">
             <img src={logo} alt="logo-tickitz" className='w-20' />
           </a>
-          <div className='flex gap-3'>
+          <div className='flex gap-3 items-center'>
             <Link to={'/'}>
-              <span className='text-white font-bold'>HOME</span>
+              <span className={`text-white font-bold ${location.pathname === '/' && 'border-b-2 border-sunburst text-xl'}`}>HOME</span>
             </Link>
             <Link to={'/movie'}>
-              <span className='text-white font-bold'>MOVIE</span>
+              <span className={`text-white font-bold ${location.pathname === '/movie' && 'border-b-2 border-sunburst text-xl'}`}>MOVIE</span>
             </Link>
-            <Link to={'/movie:id'}>
-              <span className='text-white font-bold'>BUY TICKET</span>
-            </Link>
-          </div>
-          <div className='flex gap-3'>
-            <Link to={'/auth'}>
-              <span className='text-white font-bold'>
-                LOGIN
-              </span>
-            </Link>
-            <Link to={'/auth/register'} className='flex items-center gap-1'>
-              <span className='text-white font-bold'>
-                SIGN UP
-              </span>
-              <LuSquareArrowOutUpRight className='text-white' />
+            <Link to={'/order/:id'}>
+              <span className={`text-white font-bold ${location.pathname === '/movie:id' && 'border-b-2 border-sunburst text-xl'}`}>BUY TICKET</span>
             </Link>
           </div>
+          {currentUser.length === 0 ?
+            <div className='flex gap-3'>
+              <Link to={'/auth/login'}>
+                <span className='text-white font-bold'>
+                  LOGIN
+                </span>
+              </Link>
+              <Link to={'/auth/register'} className='flex items-center gap-1'>
+                <span className='text-white font-bold'>
+                  SIGN UP
+                </span>
+                <LuSquareArrowOutUpRight className='text-white' />
+              </Link>
+            </div> :
+            <div className='flex gap-3'>
+              <Link to={'/'}>
+                <span className='text-white font-bold'>
+                  DASHBOARD
+                </span>
+              </Link>
+              <button onClick={handleLogout} className='flex items-center gap-1'>
+                <span className='text-white font-bold'>
+                  LOGOUT
+                </span>
+                <LuSquareArrowOutUpRight className='text-white' />
+              </button>
+            </div>}
         </div>
       </nav>
     </header>
