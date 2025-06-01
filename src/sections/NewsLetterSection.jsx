eb205342ce4eb1358fda4946e7b6f48afd52b0fc
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-01 12:04:14 +0700

    fix: adjust lines for balancing component

--- src/sections/NewsLetterSection.jsx
+++ src/sections/NewsLetterSection.jsx
@@ -1,15 +1,15 @@
 function NewsLetterSection() {
   return (
-    <div className="flex flex-col items-center bg-jet-black h-screen justify-center">
+    <div className="flex flex-col items-center bg-jet-black py-20 justify-center">
       <div className="flex flex-col items-center p-6 gap-6 max-w-[1080px] text-center bg-graphite w-full rounded-xl">
         <h1 className="text-ash font-semibold">Subscribe to Our Newsletter</h1>
         <form className="flex flex-col gap-3 mb-6">
           <div className="flex gap-3">
-            <input type="text" name="firstName" id="firstName" placeholder="Your First Name" className="border-ash border text-ash px-5 py-3 rounded" />
-            <input type="email" name="email" id="email" placeholder="Your Email Address" className="border-ash border text-ash px-5 py-3 rounded" />
+            <input type="text" name="firstName" id="firstName" placeholder="Your First Name" className="border-ash border text-ash px-5 py-3 rounded outline-0" />
+            <input type="email" name="email" id="email" placeholder="Your Email Address" className="border-ash border text-ash px-5 py-3 rounded outline-0" />
           </div>
           <div>
-            <button type="submit" className="w-full shadow-md px-5 py-3 rounded text-white bg-jet-black">
+            <button type="submit" className="w-full shadow-md px-5 py-3 rounded text-white bg-jet-black hover:cursor-pointer hover:bg-marigold">
               Subscribe Now
             </button>
           </div>
