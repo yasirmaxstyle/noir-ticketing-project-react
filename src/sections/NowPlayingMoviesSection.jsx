e02595a41aca93155e0cd2fb6864b6922edea3b2
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-01 12:01:33 +0700

    feat: add scroll button function for sliding horizontally image

--- src/sections/NowPlayingMoviesSection.jsx
+++ src/sections/NowPlayingMoviesSection.jsx
@@ -1,42 +1,76 @@
-import { useEffect, useState } from "react";
-import MovieCards from "../components/MovieCards";
+import { useEffect, useRef, useState } from "react";
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRightLong } from "react-icons/fa6";
-import getTrendingToday from "../api/getTrendingToday";
+import { Link } from "react-router";
+
+import MovieCards from "../components/MovieCards";
+import getMovieList from "../api/getMovieList";
 
 function NowPlayingMoviesSection() {
   const [movie, setMovie] = useState([])
+  const [arrowDisable, setArrowDisable] = useState(true)
+  const [loading, setLoading] = useState(true)
+  const cards = useRef()
+
+  function handleScroll(element, speed, distance, step) {
+    let scrollAmount = 0;
+    const slideTimer = setInterval(() => {
+      element.scrollLeft += step
+      scrollAmount += Math.abs(step);
+      if (scrollAmount >= distance) {
+        clearInterval(slideTimer);
+      }
+      if (element.scrollLeft === 0) {
+        setArrowDisable(true)
+      } else {
+        setArrowDisable(false)
+      }
+    }, speed)
+  }
 
   useEffect(() => {
-    getTrendingToday().
+    getMovieList('movie/now_playing').
       then(data => {
         setMovie(data)
+        setLoading(false)
       })
   }, [])
+
   return (
-    <div className="flex flex-col justify-center items-center bg-jet-black h-screen">
-      <div className="flex flex-col p-6 gap-6 max-w-[1080px] text-center">
+    <div className="flex flex-col justify-center items-center bg-jet-black" >
+      <div className={`flex flex-col p-6 gap-6 max-w-[1080px] w-full text-center`}>
         <h1 className="text-ash font-semibold">Now Showing in Cinemas</h1>
-        <div className="flex gap-3 overflow-scroll">
-          {movie.map(movie =>
-            <>
-              <MovieCards category="now-playing" title={movie.title} genres={movie.genre} src={movie.img} id={movie.id} />
-            </>
-          )}
-        </div>
+        {loading ?
+          <div className="w-full flex gap-3 h-[565px]">
+            {Array.from({ length: 4 }, (_, idx) =>
+              <div key={`skeleton-nowplaying-${idx}`} className="rounded-xl shrink-0 h-full basis-[calc((1/4*100%)-12px)] animate-pulse bg-platinum" />)}
+          </div> :
+          <div ref={cards} className="flex gap-3 overflow-hidden">
+            {movie.map(movie =>
+              <>
+                <MovieCards key={`now-playing-${movie.id}`} category="now-playing" title={movie.title} genres={movie.genre} src={movie.img} id={movie.id} />
+              </>
+            )}
+          </div>
+        }
         <div className="flex justify-between px-5">
           <div className="flex gap-3">
-            <button>
-              <FaArrowLeft className="text-white text-4xl" />
+            <button onClick={() =>
+              handleScroll(cards.current, 100, cards.current.clientWidth, -cards.current.clientWidth)}
+              disabled={arrowDisable}
+              className="hover:cursor-pointer disabled:text-ash text-sunburst">
+              <FaArrowLeft className="text-4xl" />
             </button>
-            <button>
-              <FaArrowRight className="text-white text-4xl" />
+            <button onClick={() =>
+              handleScroll(cards.current, 250, cards.current.clientWidth, cards.current.clientWidth)}
+              className="hover:cursor-pointer disabled:text-ash text-sunburst">
+              <FaArrowRight className="text-4xl" />
             </button>
           </div>
-          <div className="w-fit self-center flex gap-3 items-center">
-            <h5 className="text-white font-bold rounded-full">VIEW ALL</h5>
-            <FaArrowRightLong className="text-white" />
+          <div className="w-fit self-center flex gap-3 items-center text-white hover:text-sunburst">
+            <Link to={'/movie'} className="font-bold rounded-full">VIEW ALL</Link>
+            <FaArrowRightLong />
           </div>
         </div>
       </div>
