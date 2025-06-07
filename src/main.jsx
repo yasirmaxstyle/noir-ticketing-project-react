836dea73995049a1d98cdffc339b305ca3e09bdc
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-08 01:31:01 +0700

    feat: move redux provider to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,30 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, useLocation } from 'react-router'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import Router from './Router.jsx'
+import { persistor, store } from './redux/store.js'
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Router />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <ScrollToTop />
+          <Router />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
