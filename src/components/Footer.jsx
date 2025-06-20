614e03282253c1f7f71127479dae094d21064798
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-20 15:28:11 +0700

    feat: add gradient background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,14 +10,14 @@ import hiflix from "../assets/sponsor/hiflix.svg"
 function Footer() {
   return (
     <footer>
-      <div className='bg-graphite flex flex-col items-center justify-center p-6 gap-6'>
-        <div className='grid md:grid-cols-4 max-w-[1080px] w-full gap-6'>
-          <div className='text-white grid gap-3'>
-            <img src={logo} alt="logo tickitz" className='w-50' />
-            <p>Stop waiting in line. Buy tickets conveniently, watch movies quietly.</p>
+      <div className='flex flex-col items-center justify-center p-6 gap-6 bg-radial-[at_50%_100%] from-graphite to-jet-black to-50%'>
+        <div className='grid md:flex max-w-[1080px] w-full gap-6'>
+          <div className='text-white grid gap-3 flex-1'>
+            <img src={logo} alt="logo tickitz" className='w-30' />
+            <p>Stop waiting in line.<br/> Buy tickets conveniently,<br/> watch movies quietly.</p>
           </div>
-          <div className='text-white grid gap-2'>
-            <h4 className='font-bold'>EXPLORE</h4 >
+          <div className='text-white grid'>
+            <h5 className='font-bold'>EXPLORE</h5 >
             <ul>
               <li>Cinemas</li>
               <li>Movie List</li>
@@ -25,14 +25,14 @@ function Footer() {
               <li>Notification</li>
             </ul>
           </div>
-          <div className='text-white grid gap-2'>
-            <h4 className='font-bold'>OUR SPONSOR</h4 >
+          <div className='text-white grid'>
+            <h5 className='font-bold'>OUR SPONSOR</h5 >
             <img src={ebv} alt="ebv logo" />
             <img src={cineone} alt="cineone logo" />
             <img src={hiflix} alt="hiflix logo" />
           </div>
-          <div className='text-white grid gap-2'>
-            <h4 className='font-bold'>FOLLOW US</h4 >
+          <div className='text-white grid'>
+            <h5 className='font-bold'>FOLLOW US</h5 >
             <a href="" className='flex items-center gap-3'>
               <FaFacebook />
               <span>noir.cinema.id</span>
