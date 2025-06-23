92686784f468d22269aac6e950ff7cea81b8c80b
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-23 21:44:06 +0700

    feat: change background adjustment using gradient

--- src/components/MovieCards.jsx
+++ src/components/MovieCards.jsx
@@ -12,7 +12,7 @@ export default function MovieCards({ category, title, genres, release, src, id }
   })
 
   return (
-    <div className="flex grow flex-col gap-3 items-center shadow-xl rounded-xl shrink-0 basis-[calc((1*100%)-12px)] sm:basis-[calc((1/2*100%)-12px)] md:basis-[calc((1/3*100%)-12px)] lg:basis-[calc((1/4*100%)-12px)] bg-graphite group">
+    <div className="flex grow flex-col gap-3 items-center rounded-xl shrink-0 basis-[calc((1*100%)-12px)] sm:basis-[calc((1/2*100%)-12px)] md:basis-[calc((1/3*100%)-12px)] lg:basis-[calc((1/4*100%)-12px)] group bg-radial-[at_25%_45%] from-graphite to-jet-black to-65% border-graphite border">
       <div className="w-full overflow-hidden rounded-xl relative">
         <div className="hidden bg-black opacity-50 absolute w-full h-full z-[1] group-hover:block" />
         <div className="hidden absolute inset-0 translate-y-1/3 group-hover:grid justify-center z-[2]">
@@ -38,7 +38,7 @@ export default function MovieCards({ category, title, genres, release, src, id }
         </div>
         <div className="w-full h-full flex justify-center items-center text-center">
           <ImageWithFallback
-            src={src}
+            src={src || null}
             alt={title}
             loading="lazy"
             className="group-hover:scale-110 group-hover:rotate-1 ease-in duration-300 grayscale group-hover:grayscale-0 aspect-2/3 object-cover"
@@ -50,7 +50,7 @@ export default function MovieCards({ category, title, genres, release, src, id }
         <div className="flex gap-2 flex-wrap py-3">
           {genres?.map(genre => {
             return (
-              <span key={`${category}${genre}`} className="text-graphite bg-ash rounded-full px-3 py-0.5">{genre}</span>
+              <span key={`${category}${genre}`} className="text-ash border-ash border rounded-full px-3 py-0.5">{genre}</span>
             )
           })
             || <span className="text-ash">{convertDate(release, false)}</span>}
